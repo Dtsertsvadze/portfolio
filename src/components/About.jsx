e35470e1de94b5b5a832169142c388a4a2a7c986
Dtsertsvadze <tsertvadzedata97@gmail.com>
2023-09-05 09:31:34 +0400

    correct typos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,16 +11,16 @@ const About = () => {
       <div className={classes["about-wrapper"]}>
         <div className={classes["img-wrapper"]}>My profile Picture</div>
         <div className={classes["about-description"]}>
-          I am Data Tsertsvadze, begginer react developer. I have started
+          I am Data Tsertsvadze, beginner react developer. I have started
           self-educational journey in 2023 and every day with hard-work I am
           moving forward to my goals to become React Developer and land my first
           job in Tech industry.
           <br></br>I am looking forward to find a company which will give me an
           opportunity to become the member of their environment and strengthen
           my skills there, because it is crucial for my development to reach my
-          goals and become proffesional React developer<br></br>
+          goals and become professional React developer<br></br>
           With my dedication, I am certain that nothing is impossible to
-          achieve!
+          achieve.
         </div>
       </div>
     </div>
